46847ffee0443aa1e2d00b8ca5ee3f7a7759724e
Steven Helferich <steven.helferich@gmail.com>
2016-10-18 11:04:52 -0700

    base version of chatgrader

---
+++ main.py
@@ -0,0 +1,19 @@
+from sys import argv
+
+def main(argv):
+	filename_names = argv[1]
+	filename_source = argv[2]
+	names = {}
+	with open(filename_names) as f:
+		for line in f:
+			names[line.strip("\n")] = 0
+	with open(filename_source) as f:
+		for line in f:
+			for word in line.split():
+				word = word.strip("\n")
+				if word in names:
+					names[word] = names[word] + 1
+	print(names)
+
+if __name__ == "__main__":
+	main(argv)
